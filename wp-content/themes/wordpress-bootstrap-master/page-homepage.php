edd69a53a599188ec7d71609c20de2e8f18e4582
Laura Mann <lmann2@oak.dreamhost.com>
2013-08-16 21:14:16 -0700

    slider buttons to spec

--- wp-content/themes/wordpress-bootstrap-master/page-homepage.php
+++ wp-content/themes/wordpress-bootstrap-master/page-homepage.php
@@ -64,8 +64,8 @@
 					    </div>
 
 					    <!-- Carousel nav -->
-					    <a class="carousel-control left" href="#myCarousel" data-slide="prev">&lsaquo;</a>
-					    <a class="carousel-control right" href="#myCarousel" data-slide="next">&rsaquo;</a>
+					    <a class="carousel-control left" href="#myCarousel" data-slide="prev"></a>
+					    <a class="carousel-control right" href="#myCarousel" data-slide="next"></a>
 				    </div>
 
 				    <?php } // ends the if use carousel statement ?>
